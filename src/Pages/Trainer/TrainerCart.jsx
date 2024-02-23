d94ff8b1f4b66f15e1d37f655d43cd4d45c91974
MacBook Pro <macbookpro@MacBooks-MBP.Dlink>
2024-02-23 15:16:03 +0600

    create and update a trainer || card section route ||

--- src/Pages/Trainer/TrainerCart.jsx
+++ src/Pages/Trainer/TrainerCart.jsx
@@ -1,47 +1,63 @@
 /* eslint-disable react/prop-types */
-import { FaFacebook, FaInstagram, FaLinkedin, FaTwitter, FaYoutube } from "react-icons/fa";
+import {
+  FaFacebook,
+  FaInstagram,
+  FaLinkedin,
+  FaTwitter,
+  FaYoutube,
+} from "react-icons/fa";
 import Button from "../../Components/Button/Button";
 import { Link } from "react-router-dom";
 import { MdVerified } from "react-icons/md";
 import { CiTimer } from "react-icons/ci";
 
 const TrainerCart = ({ info }) => {
-  const { trainer_experience, trainer_image, trainer_name, week, day, _id } = info || {};
+  const { trainer_experience, trainer_image, trainer_name, week, day, _id } =
+    info || {};
 
   return (
     <div>
       <section>
         <section className="border border-green-500 rounded-2xl">
           <div>
-            <img className="rounded-t-2xl w-full h-64" src={trainer_image} alt="" />
+            <img
+              className="rounded-t-2xl w-full h-64"
+              src={trainer_image}
+              alt=""
+            />
           </div>
           <div className="p-4">
             <div className="flex justify-between">
               <div className="space-y-2">
-                <h3 className="text-white font-bold text-xl flex items-center gap-2">
+                <h3 className=" text-slate-900 font-bold text-xl flex items-center gap-2">
                   {trainer_name} <MdVerified className="text-blue-600" />
                 </h3>
                 <h4>
-                  Experience <span className="text-green-500">{trainer_experience}+</span> Years
+                  Experience{" "}
+                  <span className="text-green-500">{trainer_experience}+</span>{" "}
+                  Years
                 </h4>
                 <h2 className="font-bold flex items-center gap-2">
                   Available Time <CiTimer className="text-xl text-green-400" />
                 </h2>
                 <div className="flex items-center gap-2">
                   <p className="text-sm border-r-2 pr-2">
-                    Weekly <span className="text-green-400 font-bold">{week}</span> Hour
+                    Weekly{" "}
+                    <span className="text-green-400 font-bold">{week}</span>{" "}
+                    Hour
                   </p>
                   <p className="text-sm">
-                    Daily <span className="text-green-400 font-bold">{day}</span> Hour
+                    Daily{" "}
+                    <span className="text-green-400 font-bold">{day}</span> Hour
                   </p>
                 </div>
               </div>
-              <div className=" space-y-3 hover:cursor-pointer">
-                <FaFacebook className="text-[#1877F2]" />
-                <FaTwitter className="text-[#1DA1F2]" />
-                <FaLinkedin className="text-[#0A66C2]" />
-                <FaYoutube className="text-red-500" />
-                <FaInstagram className="text-[#E1306C]" />
+              <div className=" space-y-3 hover:cursor-pointer ">
+                <FaFacebook className="text-[#1877F2] trainer-icons" />
+                <FaTwitter className="text-[#1DA1F2] trainer-icons" />
+                <FaLinkedin className="text-[#0A66C2] trainer-icons" />
+                <FaYoutube className="text-red-500 trainer-icons" />
+                <FaInstagram className="text-[#E1306C] trainer-icons" />
               </div>
             </div>
 
