02b242f358ea33fe6ff622279b7a8d11f2b3d2ae
MacBook Pro <macbookpro@MacBooks-MBP.Dlink>
2024-03-01 19:13:44 +0600

    create a admin dashboard under all forums card components

--- src/Dashboard/Admin/AdminMenus.jsx
+++ src/Dashboard/Admin/AdminMenus.jsx
@@ -1,4 +1,4 @@
-import { FaUser, FaUsers, FaWallet } from "react-icons/fa";
+import { FaUser, FaUsers, FaWallet, FaForumbee } from "react-icons/fa";
 import MenuItem from "../Components/MenuItem";
 import { FcBusinessman } from "react-icons/fc";
 import { MdForum} from "react-icons/md";
@@ -12,7 +12,7 @@ const AdminMenus = () => {
           <MenuItem icon={FcBusinessman} label="All Trainers" address="/dashboard/allTrainers" />
           <MenuItem icon={FaUser} label="Applied Trainer" address="/dashboard/appliedTrainer" />
           <MenuItem icon={MdForum} label="Add new Forum" address="/dashboard/addNewForum" />
-          <MenuItem icon={MdForum} label="All Forum's" address="/dashboard/allForum" />
+          <MenuItem icon={FaForumbee} label="All Forum's" address="/dashboard/allForum" />
           <MenuItem icon={FaWallet} label="Balance" address="/dashboard/balance" />
         </nav>
       </>
--- src/Dashboard/Admin/AllForum.jsx
+++ src/Dashboard/Admin/AllForum.jsx
@@ -1,12 +1,56 @@
+import { Helmet } from "react-helmet";
 import useForum from "../../Hooks/useForum";
+import MenuListComposition from "./MenuListComposition";
 
 const AllForum = () => {
   const [allForum] = useForum();
 
+  //const { content, text, texts, photoURL, time, displayName, role } = forum || {};
+
   return (
-    <div>
-      <p>{allForum?.length}</p>
-    </div>
+    <>
+      <Helmet>
+        <title>Fintex-Fitness || All Forum</title>
+      </Helmet>
+      <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 font-josefin">
+        {allForum?.map((forum) => (
+          <section
+            key={forum._id}
+            className=" border rounded-md p-2 shadow-lg space-y-4"
+          >
+            <div className=" flex items-center justify-between">
+              <div className="avatar">
+                <div className="w-20 rounded-full">
+                  <img src={forum?.photoURL} />
+                </div>
+              </div>
+              <div>
+                <MenuListComposition />
+              </div>
+            </div>
+            <div>
+              <h3 className="text-white font-bold">{forum?.displayName}</h3>
+              <p>
+                <span className="text-[15px] font-bold">Published:</span>{" "}
+                <span className="font-bold text-green-700">{forum?.role}</span>
+              </p>
+              <p className="text-sm mt-1">{forum?.time}</p>
+            </div>
+            <div>
+              <p className="text-xl font-bold text-green-700">{forum?.texts}</p>
+            </div>
+            <div className=" space-y-3">
+              <p>{forum?.content}</p>
+              <h4>{forum?.text}</h4>
+            </div>
+            <div className=" text-slate-50 flex items-center justify-evenly font-bold rounded-md bg-slate-600 p-1">
+                <div>Total Like: <span className=" text-green-500 underline">300</span></div>
+                <div>Total Dislike: <span className="underline text-red-500">300</span></div>
+            </div>
+          </section>
+        ))}
+      </section>
+    </>
   );
 };
 
