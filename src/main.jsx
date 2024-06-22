77f4a78f65e507962fc86e5c8d3c206330654130
MacBook Pro <naym100m@gmail.com>
2024-06-22 16:41:12 +0600

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import AuthProvider from "./Provider/Authprovider.jsx";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./Routes/MainRout";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "react-hot-toast";
+import AuthProvider from "./Provider/AuthProvider";
 
 const queryClient = new QueryClient();
 
